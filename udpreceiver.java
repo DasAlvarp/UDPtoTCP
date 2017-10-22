de7b551fb11860faa42d15c0b380ebb921a3b246
alvarpq <alvarogudis@gmail.com>
2017-10-22 15:21:42 -0400

    Getters and setters are good, just have to do like...everything else

--- udpreceiver.java
+++ udpreceiver.java
@@ -2,20 +2,84 @@ import java.net.DatagramPacket;
 import edu.utulsa.unet.UDPSocket; //import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class udpreceiver {
-	static final int PORT = 32456;
+public class udpreceiver implements edu.utulsa.unet.RReceiveUDPI {
+	
+	int port = 32456;
+	int mode;
+	long modeParameter;
+	String filename;
+
 	public static void main(String[] args)
+	{
+
+	}
+
+	public boolean setMode(int mode)
+	{
+		this.mode = mode;
+		return this.mode > 0;
+	}
+
+	public int getMode()
+	{
+		return mode;
+	}
+
+	public boolean setModeParameter(long n)
+	{
+		modeParameter = n;
+		return true;
+	}
+	
+	public long getModeParameter()
+	{
+		return modeParameter;
+	}
+	
+	public void setFilename(String fname)
+	{
+		filename = fname;
+	}
+
+	public String getFilename()
+	{
+		return filename;
+	}
+
+	public boolean setLocalPort(int port)
+	{
+		if(port > 10000)
+		{
+			this.port = port;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public int getLocalPort()
+	{
+		return port;
+	}
+
+	public boolean receiveFile()
 	{
 		try
 		{
 			byte [] buffer = new byte[11];
-			UDPSocket socket = new UDPSocket(PORT);
+			UDPSocket socket = new UDPSocket(port);
 			DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
 			socket.receive(packet);
 			InetAddress client = packet.getAddress();
-			System.out.println(" Received'"+new String(buffer)+"' from " 
-+packet.getAddress().getHostAddress()+" with sender port "+packet.getPort());
+			System.out.println("Received'" + new String(buffer) + "' from " + packet.getAddress().getHostAddress() + " with sender port " + packet.getPort());
+			return true;
 		}
-		catch(Exception e){ e.printStackTrace(); }
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
--- udpsender.java
+++ udpsender.java
@@ -2,12 +2,106 @@ import java.net.DatagramPacket;
 import edu.utulsa.unet.UDPSocket; 
 //import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
-public class udpsender {
+public class udpsender implements edu.utulsa.unet.RSendUDPI
+{
 	static final String SERVER = "localhost";
 	static final int PORT = 32456;
 
-	public static void main(String[] args)
+	int mode;
+	long modeParameter;
+	String filename;
+	long timeout;
+	int localPort;
+	InetSocketAddress receiver;
+
+	public udpsender(){
+		mode = 0;
+		timeout = 100;
+		localPort = 32456;
+	}
+
+	public boolean setMode(int mode)
+	{
+		this.mode = mode;
+		return mode > 0;
+	}
+
+	public int getMode()
+	{
+		return mode;
+	}
+
+	public boolean setModeParameter(long n)
+	{
+		modeParameter = n;
+		return true;
+	}
+
+	public long getModeParameter()
+	{
+		return modeParameter;
+	}
+
+	public void setFilename(String filename)
+	{
+		this.filename = filename;
+	}
+
+	public String getFilename()
+	{
+		return filename;
+	}
+
+	public boolean setTimeout(long timeout)
+	{
+		if(timeout > 0)
+		{
+			this.timeout = timeout;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public long getTimeout()
+	{
+		return timeout;
+	}
+
+	public boolean setLocalPort(int port)
+	{
+		if(port > 10000)
+		{
+			localPort = port;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public int getLocalPort()
+	{
+		return localPort;
+	}
+
+	public boolean setReceiver(InetSocketAddress receiver)
+	{
+		this.receiver = receiver;
+		return true;
+	}
+
+	public InetSocketAddress getReceiver()
+	{
+		return receiver;
+	}
+
+	public boolean sendFile()
 	{
 		try {
 
@@ -16,7 +110,13 @@ public class udpsender {
 			//DatagramSocket socket = new DatagramSocket(23456);
 			socket.send(new DatagramPacket(buffer, buffer.length,
  				InetAddress.getByName(SERVER), PORT));
+			return true;
 		}
-		catch(Exception e){ e.printStackTrace(); }
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+
 	}
 }
